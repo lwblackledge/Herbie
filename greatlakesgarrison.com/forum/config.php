a9d7c25252b984abe0ae00071551f9a699c7c945
TS-43723 <trooper43723@gmail.com>
2019-12-14 00:10:29 -0500

    comiit parts of the forum directory

---
+++ greatlakesgarrison.com/forum/config.php
@@ -0,0 +1,18 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysql'; //database type it may not be mysql
+$dbhost = 'db.greatlakesgarrison.com';
+$dbport = '';
+$dbname = 'glgforum';
+$dbuser = 'glgdb';
+$dbpasswd = 'scarif_terrible_it_security';
+
+$table_prefix = 'phpbb3_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+//@define('DEBUG', true);
+//@define('DEBUG_EXTRA', true);
+?>
